005529615eeed68e35a594ce13feb443be0018bd
Austin <copeman.austin@gmail.com>
2016-04-02 12:41:53 -0400

    Updates
    -alt img was causing issues removed
    -Updated sql statement for movies
    -Updated sql database

--- SQL/akcopema_DB.sql
+++ SQL/akcopema_DB.sql
@@ -206,20 +206,6 @@ UNLOCK TABLES;
 -- Final view structure for view `new_view`
 --
 
-/*!50001 DROP VIEW IF EXISTS `new_view`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `new_view` AS select `t1`.`MOVIE_NAME` AS `MOVIE_NAME`,`t1`.`MOVIE_ID` AS `MOVIE_ID`,`t1`.`SHOWTIME_ID` AS `SHOWTIME_ID`,`t1`.`TIME_START` AS `TIME_START`,`t1`.`TIME_END` AS `TIME_END` from (select `akcopema`.`showtime`.`SHOWTIME_ID` AS `SHOWTIME_ID`,`akcopema`.`movie`.`MOVIE_ID` AS `MOVIE_ID`,`akcopema`.`showtime`.`TIME_START` AS `TIME_START`,`akcopema`.`showtime`.`TIME_END` AS `TIME_END`,`akcopema`.`movie`.`MOVIE_NAME` AS `MOVIE_NAME` from ((`akcopema`.`movie_times` join `akcopema`.`showtime` on((`akcopema`.`movie_times`.`SHOWTIME_ID` = `akcopema`.`showtime`.`SHOWTIME_ID`))) join `akcopema`.`movie` on((`akcopema`.`movie`.`MOVIE_ID` = `akcopema`.`movie_times`.`MOVIE_ID`)))) `t1` where (`t1`.`MOVIE_NAME` = 'The Dark Knight') */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
