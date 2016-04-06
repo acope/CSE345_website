261be1c0f08b893b0982b9187a5e17f43ac513d3
Austin <copeman.austin@gmail.com>
2016-04-06 15:08:47 -0400

    update
    something

--- Reservation.php
+++ Reservation.php
@@ -1,10 +1,16 @@
-<?php 
-    session_start();
-    require_once('php_helper/opendb.php');
+<?php
+session_start();
+include "php_helper/function.php";
+$movieInfo = getMovieInformation($_GET['name']);
+$orderedTickets = getTotalOrderedTickets($_GET['id'],$_GET['name']);
+$ticketsRemaining = 20 - $orderedTickets;
+//Retrieve varaibles from url
+$movieTime = $_GET['id']; 
+$movieName = $_GET['name'];
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 
 <head>
 
@@ -53,92 +59,62 @@
     <!--Main section will include movie photo, order details, the name of the moivie, and the time and date. It will also, include the ticket order and quantity.-->
     <!--The boarders are for looks only right now, we can change them or get rid of them-->
     
-    <section id="main">
         <div class="container">
-            <div class="row ">
+            <div class="row">
                 <div class="col-lg-8">
-                    <!--This could be populated using for loops in PHP to keep times and movie next to each other or maybe use a drop down menu -->
-                    <div class="equalHeight">
-                        
-                         
-                        <div class="col-lg-4">
-                            <img width="182" height="268" src = "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX640_SY720_.jpg" alt="The Dark Knight">
-                             
-                        </div>
-                        
-                        <div class="col-lg-8">
-                            <iframe width="560" height="268"  src="https://www.youtube.com/embed/EXeTwQWrcwY" frameborder="0" allowfullscreen></iframe>
-                           
-                        </div>
+                    <div class="equalHeight"> 
+                        <div class="row" style="padding: 10px">
+                            <div class="col-lg-4">
+                                <img width="182" height="268" src="<?php echo getMovieThumbnail($_GET['name']) ?>">                        
+                            </div>           
+                            <div class="col-lg-8">
+                                <?php echo $movieInfo[8] ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-                
-            </div>  
-        
-    </section>
-
-    <section id= "Ticket_count">
-   <form>
-        <div class="container">
-  <div class="form-group">
-      <div class="row">
-<div class="col-lg-2">
-          <div class="form-group">
-            <label for="sel1">How many tickets:</label>
-            <select class="form-control" id="sel1">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-        </select>
-        </div> 
-          </div>
-      <!-- <div class="col-lg-2">
-    <label for="Ticket_input">Ticket Quanity</label>
-    <input type="Tickets" class="form-control" id="Ticket_input" placeholder="0">
-  </div> -->
-            </div>   
-      </div>
-       </div>
-       
-</form>
-         </section>
-    
-    
-    <section id= "submit_button">
-         <div class="container">
-      <form>
-  <div class="form-group">
-      <div class="row">
-        <div class="row-lg-8">
-<button type="submit" class="btn btn-default">Reserve tickets</button>
-          </div>
-          
-      </div>
-          </div>
-          </div>
-
-       </section>
-    </form>
-   <!--  <setion id = "Ticket_Order">
-   
-        <div class = "container">
-       
-        <div class = "row equalHeight">
-       <div class="col-lg-8">  
-            <div class = "col-lg-8 movie-table">
-           <p> Ticket Order and quantity </p>
-            
             </div>
-         </div>
         </div>
         
-        </div>
-    </setion>-->
+    <form>
+        <div class="container">
+          <div class="form-group">
+              <div class="row">
+                <div class="col-lg-2">
+                  <div class="form-group">
+                    <label for="sel1">Researve tickets:</label>
+                    <select class="form-control" name="selectTickets">
+                        <?php
+                            for($i=1; $i <= $ticketsRemaining; $i++){
+                                echo "<option value='$i''>$i</option>";
+                            }
+                        ?>
+                    </select>
+                 </div> 
+                </div>
+              </div>   
+            </div>
+       </div>    
+    </form>
+            
+    <div class="container">
+        <form>
+            <div class="form-group">
+                <div class="row">
+                    <div class="row-lg-8">
+                        <button type="submit" name="submit" formmethod="post" class="btn btn-default">Reserve tickets</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+<?php
+    if(isset($_POST['submit'])){
+        echo"hello world";
+    }
+?>
     
     
-   
      <script>
         $('.equalHeight').each(function() {
         var eHeight = $(this).innerHeight();
