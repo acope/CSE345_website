db09043bf2239219d573fe837c183186e2ac0c33
Austin <copeman.austin@gmail.com>
2016-04-07 12:04:09 -0400

    Update Login button

--- php_helper/login_button.php
+++ php_helper/login_button.php
@@ -1,4 +1,5 @@
 <?php
+    $string = "/editReservation.phpWelcome ".$_SESSION['user_name'];
     // Test to see if the loggedin variable has been made
     // If not it creates it and sets it to 0 (Not logged in)
     if(isset($_SESSION['loggedin'])){
@@ -6,7 +7,12 @@
         if($_SESSION['loggedin'] != 1){
             echo( '<a  href="login.php" target="_self"> <h3>LOGIN</h3> </a>');
         }else{
-            echo("Welcome ".$_SESSION['user_name']." ".'<a  href="logout.php" target="_self"> <h3>LOGOUT</h3> </a>');
+            echo("Welcome ".$_SESSION['user_name'].'<a  href="logout.php" target="_self"> <h3>LOGOUT</h3> </a>');
+            if(isset($_SERVER['REQUEST_URI']) !== $string){
+                echo('<a  href="editReservation.php" target="_self"> <h6>Edit Reservation</h6> </a>');
+            }else{
+                echo('<a  href="'.$_SERVER('SERVER_NAME').'" target="_self"> <h6>Back to Home</h6> </a>');
+            }
         }
     }else{                            
         //$_SESSION['loggedin'] = 0;
